1542b19001f6bc851f3bef59f353efce00c519d5
Kevin Grieger <scuffmark@gmail.com>
2016-02-21 20:57:45 -0800

    commit everything else

---
+++ ViewModels/Manage/RemoveLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace azure_test_003.ViewModels.Manage
+{
+    public class RemoveLoginViewModel
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+    }
+}
